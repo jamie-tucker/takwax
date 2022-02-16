0586bec3782408db887ba0a05e9ddf77e1ad6b55
Jamie T <jamie@jamietucker.com>
2022-02-15 22:58:23 -0500

    parse entries and create struct

--- src/main.c
+++ src/main.c
@@ -3,19 +3,26 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MODEL_DIR "src/model/"
+#define CONTENT_DIR "src/content/"
+#define TEMPLATE_DIR "src/template/"
+
+#define ENTRY_SIZE 32
+
 #define COUNT(x) (sizeof(x) / sizeof((x)[0]))
 
 #pragma region Entry
 
 typedef struct Entry {
   int children_len;
-  char name[512];  // Maybe allocate here?
-  struct Entry *parent, *children[100];
+  char name[ENTRY_SIZE], template[ENTRY_SIZE];
+  struct Entry *parent, *children[64];
 } Entry;
 
 Entry *
 createEntry(Entry *entry, char *name) {
   strcpy(entry->name, name);
+  entry->children_len = 0;
   return entry;
 }
 
@@ -28,6 +35,8 @@ findEntry(Entry *entry, char *name, int size) {
       return &entry[i];
     }
   }
+
+  printf("Cannot find entry %s\n", name);
 
   return NULL;
 }
@@ -44,47 +53,67 @@ getfile(char *dir, char *filename, char *ext, char *op) {
   return fopen(fullpath, op);
 }
 
-int parse(Entry *entry) {
-  FILE *catFile;
+int parse_entries(Entry *entry, int *entry_len) {
+  FILE *entryFile = getfile(MODEL_DIR, "Entries", ".tsv", "r");
 
-  catFile = getfile("src/model/", "Categories", ".tsv", "r");
-
-  if (catFile == NULL) {
-    printf("Categories.tsv File Not Found: \n");
+  if (entryFile == NULL) {
+    printf("Entries.tsv File Not Found: \n");
     return 0;
   }
 
-  char line[512];
+  char line[3 * ENTRY_SIZE];
+  printf("%s", fgets(line, sizeof line, entryFile));  // skip header
 
-  while (fgets(line, sizeof line, catFile)) {
+  Entry *entryPtr = entry;
+  while (fgets(line, sizeof line, entryFile)) {
     char *token = strtok(line, "\t\n");
-    // char *name = (char *)malloc(sizeof(token));
-    // strcpy(name, token);
+    createEntry(entryPtr, token);
+    (*entry_len)++;
 
-    createEntry(entry++, token);
+    token = strtok(NULL, "\t\n");
+    entryPtr->parent = findEntry(entry, token, *entry_len);
+    entryPtr->parent->children[entryPtr->parent->children_len++] = entryPtr;
+
+    token = strtok(NULL, "\t\n");
+    strcpy(entryPtr->template, token);
+
+    entryPtr++;
   }
 
-  fclose(catFile);
+  fclose(entryFile);
+  return 1;
+}
+
+int parse(Entry *entry, int *entry_len) {
+  if (!parse_entries(entry, entry_len)) {
+    printf("Error Parsing Entries\n");
+  }
+
   return 1;
 }
 
 int main(void) {
-  Entry entries[10] = {};
+  Entry entries[10];
+  int entry_len = 0;
 
-  if (!parse(entries)) {
+  if (!parse(entries, &entry_len)) {
     printf("Parsing Error\n");
     exit(1);
   }
 
-  Entry *ep = entries;
-
+  // Print Debugs
   int i;
 
-  for (i = 0; i < COUNT(entries); i++) {
-    printf("Name: %s\n", ep->name);
-    ep->parent = findEntry(entries, entries[0].name, COUNT(entries));
-    printf("Parent: %s\n", ep->parent->name);
-    ep++;
+  for (i = 0; i < entry_len; i++) {
+    printf("Name: %s\n", entries[i].name);
+    printf("Parent: %s\n", entries[i].parent->name);
+    printf("Children: ");
+    int c;
+    for (c = 0; c < entries[i].children_len; c++) {
+      printf("%s ", entries[i].children[c]->name);
+    }
+    printf("\n");
+    printf("Template: %s\n", entries[i].template);
   }
 
   exit(0);
